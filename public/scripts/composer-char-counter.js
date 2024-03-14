d258322de1e71c055312bb4432b4f1e5750cbed7
Rebecca <rebecca90469@gmail.com>
2024-03-14 18:11:31 -0400

    Add character countdown to textbox and conditional class if counter drops below 0

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-  // --- our code goes here ---
+  const textCount = $('#tweet-text');
+
+  textCount.on('input', () => {
+    let textLength = textCount.val().length;
+    const number = $('.counter');
+    number.text(140 - textLength);
+    if ((140 - textLength) <= 0) {
+      $(number).addClass("max-exceeded");
+    } else {
+      $(number).removeClass("max-exceeded");
+    }
+  });
 });
